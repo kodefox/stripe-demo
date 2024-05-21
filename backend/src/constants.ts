d21000f36e3f696810d9e6804c2a67726f2deade
Wiwi Clarissa <wiwiclarissa@gmail.com>
2024-05-21 14:14:41 +0700

    be: add dotenv

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
-
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || '';
-
 export const API_URL = 'http://localhost:3000';
