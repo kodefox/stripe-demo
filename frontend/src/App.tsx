f66cca9a72e3ce2962978c6cd02253fba9740c69
Wiwi Clarissa <wiwiclarissa@gmail.com>
2024-05-21 14:25:52 +0700

    fe: setup chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
 export default function App() {
-  return <p>Hello World</p>;
+  return (
+    <Box margin={4} justifyContent="center">
+      <Text fontSize="xl" as="b">
+        Hello, world!
+      </Text>
+    </Box>
+  );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = document.createElement("div");
 root.id = "root";
@@ -8,6 +9,8 @@ document.body.append(root);
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
